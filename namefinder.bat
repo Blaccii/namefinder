df90b0c612f0df62083606b2074cb08be392ecc0
Sinners DEV. <152612151+Blaccii@users.noreply.github.com>
2025-02-22 19:12:08 +0100

    Update namefinder.bat

--- namefinder.bat
+++ namefinder.bat
@@ -4,15 +4,22 @@ setlocal enabledelayedexpansion
 set "outputFile=%USERPROFILE%\Desktop\car_models.txt"
 if exist "%outputFile%" del "%outputFile%"
 
+set /a count=0
+
+echo Searching for vehicles...
+
 for /r %%d in (*) do (
     if /I "%%~nxd"=="vehicles.meta" (
         for /f "tokens=2 delims=>" %%a in ('findstr /i "<modelName>" "%%d"') do (
             for /f "tokens=1 delims=<" %%b in ("%%a") do (
                 echo %%b >> "%outputFile%"
+                set /a count+=1
+                echo Vehicles found: !count!
             )
         )
     )
 )
 
-echo Fertig! Ergebnisse in %outputFile%
+echo Done! Total vehicles found: %count%
+echo Results saved in %outputFile%
 endlocal
